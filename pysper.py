d25d6261498a57bba6cde78ea30a06842a4a222b
Andy Tan <93958328+AndyT-PrimoSpade@users.noreply.github.com>
2023-06-08 09:11:35 +0800

    Update pysper.py

--- pysper.py
+++ pysper.py
@@ -12,20 +12,20 @@ print(datetime.datetime.now())
 psutil.cpu_percent(interval=1, percpu=False)
 # adjust_cpu_usage()
 
-audiofile_name = "audio.mp3"
+audiofile_name = "input/audio.mp3"
 
 filetype = ["m4a", "mp3", "mp4", "avi"]
 for element in filetype:
     if element in audiofile_name:
         output_path = convert_audio_to_wav(audiofile_name)
-        saved_name = audiofile_name.split(".")[0]
+        saved_name = audiofile_name.split("/")[1].split(".")[0]
         print(f"The {element} audio file has been convert to WAV format and saved to convert/{saved_name}.wav")
 
 # audiofile_name = "MacTrade"
 
 # convert_m4a_to_wav(f"input/{audiofile_name}.m4a", f"convert/{audiofile_name}.wav")
 
-audiofile_name = audiofile_name.split(".")[0]
+audiofile_name = audiofile_name.split("/")[1].split(".")[0]
 main = f"convert/{audiofile_name}.wav"
 
 audio_pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization",
