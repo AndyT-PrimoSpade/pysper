416dfcab92d9a9fc20122626e45f18a2815b2e0c
Andy Tan <93958328+AndyT-PrimoSpade@users.noreply.github.com>
2023-06-12 14:02:42 +0800

    Update pysper.py

--- pysper.py
+++ pysper.py
@@ -12,7 +12,8 @@ print(datetime.datetime.now())
 psutil.cpu_percent(interval=1, percpu=False)
 # adjust_cpu_usage()
 
-audiofile_name = "input/audio.mp3"
+audio_filename = input("Audio File Name: ")
+audiofile_name = f"input/{audio_filename}"
 
 filetype = ["m4a", "mp3", "mp4", "avi"]
 for element in filetype:
@@ -20,10 +21,6 @@ for element in filetype:
         output_path = convert_audio_to_wav(audiofile_name)
         saved_name = audiofile_name.split("/")[1].split(".")[0]
         print(f"The {element} audio file has been convert to WAV format and saved to convert/{saved_name}.wav")
-
-# audiofile_name = "MacTrade"
-
-# convert_m4a_to_wav(f"input/{audiofile_name}.m4a", f"convert/{audiofile_name}.wav")
 
 audiofile_name = audiofile_name.split("/")[1].split(".")[0]
 main = f"convert/{audiofile_name}.wav"
@@ -38,9 +35,9 @@ for result in tqdm(range(1)):
 
 diarized_text = diarize_and_merge_text(asr_transcription, diarization)
 
-write_results_to_txt_file(diarized_text, "output/result.txt")
+write_results_to_txt_file(diarized_text, f"output/{audiofile_name}.txt")
 
-convert_txt_to_srt("output/result.txt", "output/result.srt")
+convert_txt_to_srt(f"output/{audiofile_name}.txt", f"output/{audiofile_name}.srt")
 
 print(datetime.datetime.now())
 
