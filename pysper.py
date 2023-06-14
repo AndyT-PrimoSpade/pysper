818a62de9ca1ac5b75087606219d55e297debad6
Andy Tan <93958328+AndyT-PrimoSpade@users.noreply.github.com>
2023-06-14 16:49:45 +0800

    Update pysper.py

--- pysper.py
+++ pysper.py
@@ -9,25 +9,30 @@ import datetime
 
 print(datetime.datetime.now())
 
-psutil.cpu_percent(interval=1, percpu=False)
+# psutil.cpu_percent(interval=1, percpu=False)
 # adjust_cpu_usage()
 
 audiofile_name = has_file()
 
-filetype = ["m4a", "mp3", "mp4", "avi"]
+filetype = ["m4a", "mp3", "mp4", "avi", "wav"]
 for element in filetype:
     if element in audiofile_name:
         output_path = convert_audio_to_wav(audiofile_name)
         saved_name = audiofile_name.split("/")[1].split(".")[0]
         print(f"The {element} audio file has been convert to WAV format and saved to convert/{saved_name}.wav")
 
+# audiofile_name = "MacTrade"
+
+# convert_m4a_to_wav(f"input/{audiofile_name}.m4a", f"convert/{audiofile_name}.wav")
+
 audiofile_name = audiofile_name.split("/")[1].split(".")[0]
 main = f"convert/{audiofile_name}.wav"
 
 audio_pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization",
                                     use_auth_token="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")
-asr_model = whisper.load_model("medium.en")
-asr_transcription = asr_model.transcribe(main, verbose=False, language="en")
+asr_model = whisper.load_model("medium")
+
+asr_transcription = asr_model.transcribe(main, verbose="True", language="chinese")
 
 for result in tqdm(range(1)):
     diarization = audio_pipeline(main)
