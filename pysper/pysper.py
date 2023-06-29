f503d5b745299ee381c25d094fa24b0734cd4c0b
Andy Tan <93958328+AndyT-PrimoSpade@users.noreply.github.com>
2023-06-30 01:05:56 +0800

    Update pysper.py

--- pysper/pysper.py
+++ pysper/pysper.py
@@ -1,7 +1,7 @@
 import os
 import whisper
 from pyannote.audio import Pipeline
-from utils import diarize_and_merge_text, write_results_to_txt_file, convert_txt_to_srt, split_audio, whisper_txt_combine, combine_txt_file, adjust_cpu_usage, convert_m4a_to_wav, convert_audio_to_wav, clear_cmd, min_console, finish_popup
+from test import diarize_and_merge_text, write_results_to_txt_file, convert_txt_to_srt, adjust_cpu_usage, convert_audio_to_wav, clear_cmd
 from tqdm import tqdm
 import psutil
 import time
@@ -9,15 +9,14 @@ import datetime
 import sys
 import torch
 
-print(datetime.datetime.now())
+
 start = datetime.datetime.now()
 psutil.cpu_percent(interval=1, percpu=False)
-# device = torch.device("cuda:0")
-device = torch.device("cpu")
-# adjust_cpu_usage()
+device = torch.device("cuda:0")
+# device = torch.device("cpu")
+
 audio_filename = input("Audio File Name: ")
 audiofile_name = f"../input/{audio_filename}"
-clear_cmd()
 filetype = ["m4a", "mp3", "mp4", "avi"]
 for element in filetype:
     if element in audiofile_name:
@@ -25,32 +24,19 @@ for element in filetype:
         saved_name = audiofile_name.split("/")[-1].split(".")[0]
         print(f"The {element} audio file has been convert to WAV format and saved to convert/{saved_name}.wav")
 clear_cmd()
-# min_console()
 audiofile_name = audiofile_name.split("/")[-1].split(".")[0]
 main = f"../convert/{audiofile_name}.wav"
-clear_cmd()
 audio_pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization",
-                                    use_auth_token="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")
+                                    use_auth_token="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").to(device)
 clear_cmd()
-asr_model = whisper.load_model("medium").to(device)
-# asr_model = whisper.load_model("medium")
-# asr_model.to(torch.device("cuda"))
-# audio_pipeline.to(torch.device("cuda"))
-clear_cmd()
+asr_model = whisper.load_model("large-v2").to(device)
+asr_model.to(torch.device("cuda:0"))
+audio_pipeline.to(torch.device("cuda:0"))
 asr_transcription = asr_model.transcribe(main, verbose=False, language="en")
-clear_cmd()
 for result in tqdm(range(1)):
     diarization = audio_pipeline(main)
-clear_cmd()
 diarized_text = diarize_and_merge_text(asr_transcription, diarization)
-
 write_results_to_txt_file(diarized_text, f"../output/{audiofile_name}.txt")
-
 convert_txt_to_srt(f"../output/{audiofile_name}.txt", f"../output/{audiofile_name}.srt")
-
-print(datetime.datetime.now())
 end = datetime.datetime.now()
-
-# duration = end - start
-# finish_popup(duration)
-
+print(end-start)
