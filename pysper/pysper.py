f71e7637c44f40d9ce9a0fa714a318f74eec14d7
Andy Tan <93958328+AndyT-PrimoSpade@users.noreply.github.com>
2023-09-13 18:17:34 +0800

    Update pysper.py

--- pysper/pysper.py
+++ pysper/pysper.py
@@ -17,6 +17,7 @@ device = torch.device("cuda:0")
 # device = torch.device("cpu")
 
 audio_filename = input("Audio File Name: ")
+audio_filename = audio_filename.lower()
 audiofile_name = f"../input/{audio_filename}"
 filetype = ["m4a", "mp3", "mp4", "avi"]
 for element in filetype:
@@ -28,14 +29,14 @@ audiofile_name = audiofile_name.split("/")[-1].split(".")[0]
 main = f"../convert/{audiofile_name}.wav"
 with open("output.txt", "w") as f:
     sys.stdout = f
-    audio_pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization", use_auth_token=None).to(device)
+    audio_pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization", use_auth_token="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").to(device)
     sys.stdout = sys.__stdout__
 if os.path.exists("output.txt"):
     os.remove("output.txt")
 asr_model = whisper.load_model("large-v2").to(device)
 asr_model.to(torch.device("cuda:0"))
 audio_pipeline.to(torch.device("cuda:0"))
-asr_transcription = asr_model.transcribe(main, verbose=False, language="en")
+asr_transcription = asr_model.transcribe(main, verbose=False, language="en", task="translate")
 for result in tqdm(range(1)):
     diarization = audio_pipeline(main)
 diarized_text = diarize_and_merge_text(asr_transcription, diarization)
@@ -44,5 +45,3 @@ convert_txt_to_srt(f"../output/{audiofile_name}.txt", f"../output/{audiofile_nam
 end = datetime.datetime.now()
 print(end-start)
 clear_purge()
-
-
