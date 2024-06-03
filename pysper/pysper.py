f9d899c2a3d4966eea779aa038005e0299069d85
AndyT-PrimoSpade <andytzjsu@gmail.com>
2024-06-03 17:41:54 +0800

    updated pysper msg

--- pysper/pysper.py
+++ pysper/pysper.py
@@ -1,7 +1,7 @@
 import os
 import whisper
 from pyannote.audio import Pipeline
-from utils import diarize_and_merge_text, write_results_to_txt_file, convert_txt_to_srt, adjust_cpu_usage, convert_audio_to_wav, clear_cmd, clear_purge, print_time_difference
+from utils import diarize_and_merge_text, write_results_to_txt_file, convert_txt_to_srt, adjust_cpu_usage, convert_audio_to_wav, clear_cmd, clear_purge
 from tqdm import tqdm
 import psutil
 import time
@@ -17,6 +17,7 @@ device = torch.device("cuda:0")
 # device = torch.device("cpu")
 
 audio_filename = input("Audio File Name: ")
+audio_filename = audio_filename.lower()
 audiofile_name = f"../input/{audio_filename}"
 filetype = ["m4a", "mp3", "mp4", "avi"]
 for element in filetype:
@@ -24,20 +25,18 @@ for element in filetype:
         output_path = convert_audio_to_wav(audiofile_name)
         saved_name = audiofile_name.split("/")[-1].split(".")[0]
         print(f"The {element} audio file has been convert to WAV format and saved to convert/{saved_name}.wav")
-clear_cmd()
 audiofile_name = audiofile_name.split("/")[-1].split(".")[0]
 main = f"../convert/{audiofile_name}.wav"
 with open("output.txt", "w") as f:
     sys.stdout = f
-    audio_pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization", use_auth_token=None).to(device)
+    audio_pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization").to(device)
     sys.stdout = sys.__stdout__
 if os.path.exists("output.txt"):
     os.remove("output.txt")
-clear_cmd()
-asr_model = whisper.load_model("medium").to(device)
+asr_model = whisper.load_model("large-v2").to(device)
 asr_model.to(torch.device("cuda:0"))
 audio_pipeline.to(torch.device("cuda:0"))
-asr_transcription = asr_model.transcribe(main, verbose=False, language="en")
+asr_transcription = asr_model.transcribe(main, verbose=False, language="en", task="translate")
 for result in tqdm(range(1)):
     diarization = audio_pipeline(main)
 diarized_text = diarize_and_merge_text(asr_transcription, diarization)
@@ -45,5 +44,4 @@ write_results_to_txt_file(diarized_text, f"../output/{audiofile_name}.txt")
 convert_txt_to_srt(f"../output/{audiofile_name}.txt", f"../output/{audiofile_name}.srt")
 end = datetime.datetime.now()
 print(end-start)
-print_time_difference(start, end)
 clear_purge()
